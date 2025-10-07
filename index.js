8754c1ddb088ee5de44fa9864b78b88686011cdb
longnh <tobeshindaemon@gmail.com>
2025-10-07 23:20:29 +0700

    create app v3.2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/email", (req, res) => {
 
   console.log("Hello user!");
   res.json({
-    serviceName: "LONGAWS ECS Email Microservice V3.1",
+    serviceName: "LONGAWS ECS Email Microservice V3.2",
     clientIp,
     elbIp,
     containerIp,
